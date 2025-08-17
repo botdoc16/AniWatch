b471287ad871652b6c4688a474d437c597705515
botdoc16 <botdoc16@gmail.com>
2025-08-18 01:19:18 +0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 // https://vitejs.dev/config/
@@ -35,7 +34,6 @@ export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
     mode === 'development' &&
-    componentTagger(),
     ViteImageOptimizer({
       jpg: {
         quality: 80,
